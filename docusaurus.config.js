0194a67e093edb259b72820430255cd3bb2ba23f
Sam <sam@developersam.com>
2020-03-25 02:01:00 -0400

    Add prettier

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,18 +9,18 @@ module.exports = {
   themeConfig: {
     prism: {
       theme: require('./atom-one-light-theme'),
-      darkTheme: require('prism-react-renderer/themes/nightOwl')
+      darkTheme: require('prism-react-renderer/themes/nightOwl'),
     },
     navbar: require('./navbar'),
-    footer: require('./footer')
+    footer: require('./footer'),
   },
   presets: [
     [
       '@docusaurus/preset-classic',
       {
         docs: { sidebarPath: require.resolve('./sidebars.json') },
-        theme: { customCss: require.resolve('./src/css/custom.css') }
-      }
-    ]
-  ]
+        theme: { customCss: require.resolve('./src/css/custom.css') },
+      },
+    ],
+  ],
 };
